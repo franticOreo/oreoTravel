b912264c03b64dcdc5f0f32f4f832f8b0fce6b29
Eli <eli.simic.robertson@gmail.com>
2018-05-09 17:10:01 +0800

    adding routes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', title2: 'EXXPRESS' });
+router.get('/', function(req, res) {
+  res.render('Welcome');
 });
+
+router.post('/dash', function(req, res) {
+  res.render('Dash');
+});
+
+
 
 module.exports = router;
