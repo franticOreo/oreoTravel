73967f056182b1a239732a2cd50db58630e0867c
timtimtam <38277064+timtimtam@users.noreply.github.com>
2018-05-10 11:43:46 +0800

    Added Color and Font variables to css
    Mainly changed css, also removed some elements from the nav

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var renderText = { title: 'Oreo Travel', first: 'John', last: 'Smith'};
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('Welcome');
+  res.render('Welcome', renderText);
 });
 
 router.post('/dash', function(req, res) {
-  res.render('Dash');
+  req.
+  res.render('Dash', renderText);
 });
 
 
