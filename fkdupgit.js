dd49596710a405005fdc458c685f81edabf5dd69
Eli <eli.simic.robertson@gmail.com>
2018-05-10 12:36:41 +0800

    think I screwed up the git, fixing now

--- fkdupgit.js
+++ fkdupgit.js
@@ -37,3 +37,43 @@ router.post('/dash', function(req, res, next) {
 
 
 module.exports = router;
+
+
+//
+
+
+var mongoose = require('mongoose');
+
+var UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  expertise: {
+    type: String,
+    required: true,
+    trim: true
+  }
+
+});
+
+var User = mongoose.model('User', UserSchema); // creates model name and points to schema
+                                                // it wants to use
+module.exports = User;
