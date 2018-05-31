f1bc07d6e912f188786a2d0d1539b5b20cc6e76e
timtimtam <38277064+timtimtam@users.noreply.github.com>
2018-05-31 13:50:21 +0800

    Testing

--- test/sanity_test.js
+++ test/sanity_test.js
@@ -1,4 +1,11 @@
 var expect = require('chai').expect;
+var should = require('should')
+var assert = require('assert')
+// var DB = require('../../db')
+var fixtures = require('../models/user')
+var createUser = require('../controllers/createUser')
+var mongoose = require('mongoose')
+var User = require('../models/user');
 
 // Sanity Check
 
@@ -15,16 +22,12 @@ describe('Mocha', function () {
 
 /////////////////////////////////////////////////////////
 
-var should = require('should')
-var assert = require('assert')
-// var DB = require('../../db')
-var fixtures = require('../models/user')
-var createUser = require('../controllers/createUser')
+
 
 describe('creating user test', function () {
 
   before(function (done) {
-    db = mongoose.connect("mongodb://localhost:27017/oreoTravel");
+    db = mongoose.connect("mongodb://franticOreoMinion:lol@ds119350.mlab.com:19350/oreo_travel_db");
     done()
   })
 
@@ -34,31 +37,33 @@ describe('creating user test', function () {
   })
 
   beforeEach(function (done) {
-    var firstName = "test",
-    lastName = "test",
-    password = "lol",
-    email = "test@lol.com",
-    expertise = "rofln",
-    region = "Africa"
+    var user = { 
+      firstName: "test",
+      lastName: "test",
+      password: "lol",
+      email: "test@lol.com",
+      expertise: "rofln",
+      region: "Africa"
+    }
 
-    User.create(function(error) {
+    User.create(user, function(error) {
       if (error) console.log(error);
       else console.log('User created')
       done()
     })
+  })
 
-    it('return person data', function(done) {
-      User.find({email:"test@lol.com"}, function(err, data) {
-        assert.deepEqual([data.firstName], ['test'])
-        console.log(true)
-        done()
-      })
+  it('return person data', function(done) {
+    User.find({email:"test@lol.com"}, function(err, data) {
+      assert.deepEqual([data.firstName], ['test'])
+      console.log(true)
+      done()
     })
+  })
 
-    afterEach(function (done) {
-      User.remove({}, function() {
-        done();
-      })
+  afterEach(function (done) {
+    User.remove({}, function() {
+      done();
     })
   })
 })
--- test/user_authentication_test.js
+++ test/user_authentication_test.js
@@ -1,18 +1,18 @@
-// https://codeutopia.net/blog/2016/06/10/mongoose-models-and-unit-tests-the-definitive-guide/
+// // https://codeutopia.net/blog/2016/06/10/mongoose-models-and-unit-tests-the-definitive-guide/
 
-var expect = require('chai').expect;
-var sinon = require('sinon')
-// var test = require('sinon').test
-var User = require('../models/user.js')
+// var expect = require('chai').expect;
+// var sinon = require('sinon')
+// // var test = require('sinon').test
+// var User = require('../models/user.js')
 
 
-// set up model to fail validation
-it('User validation test', sinon.test(function () {
-  this.stub(User, 'findOne');
-  var email = 'test@test.com';
-  var u = new User({email: email});
+// // set up model to fail validation
+// it('User validation test', sinon.test(function () {
+//   this.stub(User, 'findOne');
+//   var email = 'test@test.com';
+//   var u = new User({email: email});
 
-  u.statics.authenticate(User.findOne, {
-    email: email
-  })
-}))
+//   u.statics.authenticate(User.findOne, {
+//     email: email
+//   })
+// }))
